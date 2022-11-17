29cbc7fb3a9ef7b3ee075175962d3a4bbff23e80
Liwaho Xu <lwhollow@yandex.ru>
2022-11-17 15:04:17 +0500

    New arrays

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
 ﻿// Массив из строк -> массив из строк, длина которых меньше или равна тём символам.
+Console.WriteLine("Сколько элементов в массиве? ");
+int wordsCount = int.Parse(Console.ReadLine());
+string[] words = new string[wordsCount];
+int newWordsCount = 0;
+string[] newWords = new string[newWordsCount];
+Console.WriteLine("Введите элементы массива.");
+for (int i = 0; i < wordsCount; i++)
+{
+    Console.WriteLine(i + " элемент: ");
+    words[i] = Console.ReadLine();
+}
+
