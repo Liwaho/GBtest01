b83e8ca950e619aabb1bf96e592e62856d666fba
Liwaho Xu <lwhollow@yandex.ru>
2022-11-18 13:09:13 +0500

    finalising array construction

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿// Массив из строк -> массив из строк, длина которых меньше или равна тём символам.
+
 Console.WriteLine("Сколько элементов в массиве? ");
 int wordsCount = int.Parse(Console.ReadLine());
+if (wordsCount > 10)
+{
+    Console.WriteLine("Слишком много элементов! Нужно не больше 10!");
+    return;
+}
 string[] words = new string[wordsCount];
 int newWordsCount = 0;
-string[] newWords = new string[newWordsCount];
 Console.WriteLine("Введите элементы массива.");
 for (int i = 0; i < wordsCount; i++)
 {
@@ -11,10 +16,39 @@ for (int i = 0; i < wordsCount; i++)
     words[i] = Console.ReadLine();
 }
 
+Console.WriteLine("Исходный массив: ");
 Print(words);
-Console.WriteLine("Всего " + newWordsCount + " элементов, длина которых меньше или равна трём.");
-Console.WriteLine("Это: ");
+Console.WriteLine("");
+Count(words);
+Console.WriteLine("Элементов длиной меньше или равной трём: " + newWordsCount);
+
+string[] newWords = new string[newWordsCount];
+int ni = 0;
+for (int n = 0; n < wordsCount; n++)
+    {
+        int length = words[n].Length;
+        
+        if (length < 4)
+        {   
+            newWords[ni] = words[n];
+            ni++;
+        }
+    }
+
+Console.WriteLine("Новый массив: ");
 Print(newWords);
+
+void Count(string[] array)
+{
+    foreach (string element in array)
+    {
+        int length = element.Length;
+        if (length < 4)
+        {   
+            newWordsCount++;
+        }
+    }
+}
 
 void Print(string[] el)
 {
@@ -22,7 +56,7 @@ void Print(string[] el)
     int pos = 0;
     while (pos<count)
     {
-        Console.WriteLine(el[pos] + " ");
+        Console.Write(el[pos] + " ");
         pos++;
     }
 }
